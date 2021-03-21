18b4b265a0a6c9cc29932a36904940dae8ad8c8a
wang4918 <35510669+wang4918@users.noreply.github.com>
2021-03-22 00:19:07 +0100

    added testing umap and clusering

--- utils.py
+++ utils.py
@@ -27,3 +27,22 @@ def get_monthly_stack(year, month, locid):
         arrlist.append(get_tif_as_array(path))
 
     return np.hstack(arrlist)
+
+
+
+
+
+#from utils import *
+
+#monthlystack = get_monthly_stack("2018", "04", "10N/1311_3077_13")
+
+#sns.set(style='white', rc={'figure.figsize':(10,8)})
+#standard_embedding = umap.UMAP(random_state=42).fit_transform(monthlystack)
+
+#plt.scatter(standard_embedding[:, 0], standard_embedding[:, 1],  s=0.1, cmap='Spectral');
+
+#labels = hdbscan.HDBSCAN(
+#    min_samples=10,
+#    min_cluster_size=5000,
+#).fit_predict(standard_embedding)
+#plt.scatter(standard_embedding[:, 0], standard_embedding[:, 1], c = labels,  s=0.1, cmap='Spectral')
